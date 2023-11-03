59563699079527d8cb906b16a8d6529e7edfc675
chenzirui <1842510986@qq.com>
2023-11-03 13:23:07 +0800

    1.调整加载顺序

--- assets/js/logic/home.js
+++ assets/js/logic/home.js
@@ -4,6 +4,20 @@ var CMD_LIST = []
 var CMD_QUICKVIEW = null
 
 $.ajax({
+    url: 'assets/banners.json', // json文件的路径
+    type: 'GET', // 请求的方式
+    async: false, // 同步请求
+    dataType: 'json', // 响应的数据格式
+    success: function (data) { // 成功回调函数
+        // console.log(data); // 在控制台输出读取到的数据
+        $("#tmpl2").tmpl({ dataList: data }).appendTo(".slider-section .swiper-wrapper")
+    },
+    error: function (xhr, status, error) { // 失败回调函数
+        console.error(xhr, status, error);
+    }
+});
+
+$.ajax({
     url: 'assets/commoditys.json', // json文件的路径
     type: 'GET', // 请求的方式
     async: false, // 同步请求
@@ -18,19 +32,7 @@ $.ajax({
     }
 });
 
-$.ajax({
-    url: 'assets/banners.json', // json文件的路径
-    type: 'GET', // 请求的方式
-    async: false, // 同步请求
-    dataType: 'json', // 响应的数据格式
-    success: function (data) { // 成功回调函数
-        // console.log(data); // 在控制台输出读取到的数据
-        $("#tmpl2").tmpl({ dataList: data }).appendTo(".slider-section .swiper-wrapper")
-    },
-    error: function (xhr, status, error) { // 失败回调函数
-        console.error(xhr, status, error);
-    }
-});
+
 
 function dk(e) {
     const id = $(e).attr('data-id')
